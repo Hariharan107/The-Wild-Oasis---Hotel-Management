5cd68d776a5931402e5291d4c3d168e3b9828821
Hariharan <hariharen107@gmail.com>
2023-07-12 12:30:17 -0700

    Added AppLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,38 @@
-import styled from "styled-components";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import GlobalStyles from "./styles/GlobalStyles";
-const H1 = styled.h1`
-  color: var(--color-brand-500);
-`;
-const App = () => {
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./ui/AppLayout";
+
+function App() {
   return (
     <>
       <GlobalStyles />
-      <div>
-        <H1>hello world</H1>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
+n
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
-};
+}
 
 export default App;
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,5 +1,4 @@
 import styled, { css } from "styled-components";
-
 const sizes = {
   small: css`
     font-size: 1.2rem;
@@ -19,7 +18,6 @@ const sizes = {
     font-weight: 500;
   `,
 };
-
 const variations = {
   primary: css`
     color: var(--color-brand-50);
