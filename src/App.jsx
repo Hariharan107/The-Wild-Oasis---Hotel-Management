7462bd64184eb5b1a02ee83c597c110d76adc931
Hariharan <hariharen107@gmail.com>
2023-07-14 12:05:24 -0700

    React-Query setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import GlobalStyles from "./styles/GlobalStyles";
 import Dashboard from "./pages/Dashboard";
@@ -12,25 +14,33 @@ import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
 
 function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 60, // 1 minute
+      },
+    },
+  });
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
           <Route element={<AppLayout />}>
-            <Route index element={<Navigate replace to="dashboard" />} />
-            <Route path="dashboard" element={<Dashboard />} />
-            <Route path="bookings" element={<Bookings />} />
-            <Route path="cabins" element={<Cabins />} />
-            <Route path="users" element={<Users />} />
-            <Route path="settings" element={<Settings />} />
-            <Route path="account" element={<Account />} />
+            <Route index element={<Navigate replace to='dashboard' />} />
+            <Route path='dashboard' element={<Dashboard />} />
+            <Route path='bookings' element={<Bookings />} />
+            <Route path='cabins' element={<Cabins />} />
+            <Route path='users' element={<Users />} />
+            <Route path='settings' element={<Settings />} />
+            <Route path='account' element={<Account />} />
           </Route>
-          <Route path="login" element={<Login />} />
-          <Route path="*" element={<PageNotFound />} />
+          <Route path='login' element={<Login />} />
+          <Route path='*' element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
 
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,11 +1,16 @@
+import { getCabins } from "../services/apiCabins";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-
+import { useEffect } from "react";
 function Cabins() {
+  useEffect(() => {
+    getCabins().then((cabins) => console.log(cabins));
+  }, []);
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
+    <Row type='horizontal'>
+      <Heading as='h1'>All cabins</Heading>
       <p>TEST</p>
+      <img src='https://zhwpqjjjronwhdoaffta.supabase.co/storage/v1/object/sign/cabin-images/cabin-001.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJjYWJpbi1pbWFnZXMvY2FiaW4tMDAxLmpwZyIsImlhdCI6MTY4OTMwOTM0MCwiZXhwIjoxNjg5OTE0MTQwfQ.Jc-g-4QLZKEUGDtEfYeHRWopI11jpH6xzLJgbVv3CvQ&t=2023-07-14T17%3A06%3A00.035Z' />
     </Row>
   );
 }
