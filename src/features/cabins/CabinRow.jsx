87982fa748037c9707e9861e2b54f82e119e1f44
Hariharan <hariharen107@gmail.com>
2023-07-16 17:31:07 +0530

    Added Delete functionality

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,7 +1,9 @@
 /* eslint-disable react/prop-types */
-import React from "react";
+
 import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteCabins } from "../../services/apiCabins";
 const TableRow = styled.div`
   display: grid;
   grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
@@ -42,7 +44,25 @@ const Discount = styled.div`
 `;
 
 const CabinRow = ({ cabin }) => {
-  const { name, maxCapacity, regularPrice, discount, image } = cabin;
+  const queryClient = useQueryClient();
+  const { isLoading: isDeleting, mutate } = useMutation({
+    mutationFn: deleteCabins,
+    onSuccess: () => {
+      queryClient.invalidateQueries("cabins");
+      alert("Cabin Successfully deleted");
+    },
+    onError: (error) => {
+      alert(error.message);
+    },
+  });
+  const {
+    id: cabinId,
+    name,
+    maxCapacity,
+    regularPrice,
+    discount,
+    image,
+  } = cabin;
   return (
     <TableRow role='row'>
       <Img src={image} />
@@ -50,7 +70,9 @@ const CabinRow = ({ cabin }) => {
       <div>Fits up to{maxCapacity} guests</div>
       <Price>{formatCurrency(regularPrice)}</Price>
       <Discount>{formatCurrency(discount)}</Discount>
-      <button>Delete</button>
+      <button disabled={isDeleting} onClick={() => mutate(cabinId)}>
+        Delete
+      </button>
     </TableRow>
   );
 };
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -4,7 +4,15 @@ export const getCabins = async () => {
   const { data, error } = await supabase.from("cabins").select("*");
   if (error) {
     console.error(error);
-    throw new Error(error.message);
+    throw new Error("Cabin could not be loaded");
+  }
+  return data;
+};
+export const deleteCabins = async (id) => {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+  if (error) {
+    console.error(error);
+    throw new Error("Cabin could not be deleted");
   }
   return data;
 };
