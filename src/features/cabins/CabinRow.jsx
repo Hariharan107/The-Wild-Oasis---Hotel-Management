3d0db1dc8f355adb4600b125890b703cdf3d87e3
Hariharan <hariharen107@gmail.com>
2023-08-08 10:51:05 -0700

    Added delete Models

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import { HiPencil, HiSquare2Stack, HiTrash } from "react-icons/hi2";
 import { formatCurrency } from "../../utils/helpers";
 import CreateCabinForm from "./CreateCabinForm";
+import ConfirmDelete from "../../ui/ConfirmDelete";
 import { useDeleteCabin } from "./useDeleteCabin";
 import { useCreateCabin } from "./useCreateCabin";
 import Modal from "../../ui/Modal";
@@ -80,17 +81,26 @@ const CabinRow = ({ cabin }) => {
           <HiSquare2Stack />
         </button>
         <Modal>
-          <Modal.Open opens="edit">
+          <Modal.Open opens='edit'>
             <button>
               <HiPencil />
             </button>
           </Modal.Open>
-          <Modal.Window name="edit">
-            <CreateCabinForm cabinToEdit={cabin}  />
+          <Modal.Window name='edit'>
+            <CreateCabinForm cabinToEdit={cabin} />
           </Modal.Window>
-          <button disabled={isDeleting} onClick={() => deleteCabin(cabinId)}>
-            <HiTrash />
-          </button>
+          <Modal.Open>
+            <button>
+              <HiTrash />
+            </button>
+          </Modal.Open>
+          <Modal.Window>
+            <ConfirmDelete
+              resourceName='cabins'
+              disabled={isDeleting}
+              onConfirm={() => deleteCabin(cabinId)}
+            />
+          </Modal.Window>
         </Modal>
       </div>
     </TableRow>
