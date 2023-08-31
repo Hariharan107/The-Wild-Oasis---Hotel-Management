a11049a4b217bd1fb2531d32d3c3ba491f7ba8ed
Hariharan <hariharen107@gmail.com>
2023-08-31 21:27:27 +0530

    Added API Side filtering

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,12 +1,13 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
-export const getBookings = async () => {
-  const { data, error } = await supabase
+export const getBookings = async ({ filter, sortBy }) => {
+  let query = supabase
     .from("bookings")
     .select(
-      "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)",
-      { count: "exact" }
+      "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)"
     );
+  if (filter !== null) query = query.eq(filter.field, filter.value);
+  const { data, error } = await query;
   if (error) {
     console.error(error);
     throw new Error("Bookings could not get loaded");
